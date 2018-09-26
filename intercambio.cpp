f1048894384bed506e1da00bcc57746bcd918fdf
Felipe17Programmer <hector.idme@ucsp.edu.pe>
2018-09-26 09:35:09 -0500

    punteros

--- intercambio.cpp
+++ intercambio.cpp
@@ -1,4 +1,5 @@
 //reciba puntero entero y reciba si es un año bisiesto
+//funcion si un numero es primo pero recibe numero con puntero
 #include<iostream>
 #include<conio.h>
 using namespace std;
@@ -20,6 +21,27 @@ int potenciaP(int *b, int *e){
 bool bisiestoP(int *a){
 	return (!(*a%4)&&(*a%100==0||!(*a%400==0)));
 }
+
+/*bool primoP(int *a){
+	bool primo=true;
+	int cont=2;
+	int cont1= 0;
+	for(int i=1;i<=cont;i++){
+		*a%i==0?primo=false:
+	}
+	return 0;
+}
+
+int x=10;
+int &y=x;
+*ptr= &x;
+y++;
+x++;
+*ptr +=30;
+cout<<ptr;
+cout<<*ptr;
+cout 42;
+*/
 int main(){
 	cout<<"TRABAJANDO CON PUNTEROS"<<endl;
 	int a=2;
