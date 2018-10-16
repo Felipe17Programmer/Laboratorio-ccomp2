6a8dc15d2fc031508559e7eaf65d38059cf70d9c
Felipe17Programmer <hector.idme@ucsp.edu.pe>
2018-10-16 09:36:58 -0500

    puntero a funcion

---
+++ puntero_funcion.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<conio.h>
+
+using namespace std;
+
+bool ascend(int a, int b){
+	return a<b;
+}
+
+bool descend(int a,int b){
+	return a>b;
+}
+
+void algol1(int arr[],int n,bool (*fp)(int,int)){
+	for(int i=0;i<n-1;i++){
+		int mini=i;
+		for(int j=i+1;j<n;j++){
+			if((*fp)(arr[j],arr[mini])){
+				mini=j;
+			}
+		}
+		int temp=arr[mini];
+		arr[mini]=arr[i];
+		arr[i]=temp;
+	}
+	
+	for(int i=0;i<n;i++){
+		cout<<arr[i]<<" ";
+	}
+}
+
+int main(){
+	
+	int arr[]={6,2,5,4,7,8,9,3};
+	algol1(arr,8,descend);
+	
+	getch();
+	return 0;
+}
